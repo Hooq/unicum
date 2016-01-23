2f036339cea2f96c4bd5c5eb81982d39abe841ed
Francesco Sullo <sullof@gmail.com>
2016-01-23 15:17:09 -0800

    Updating cli

--- run_redis_cli.sh
+++ run_redis_cli.sh
@@ -9,5 +9,5 @@ fi
 sudo docker run -ti \
     --name redis_cli \
     --link unicum_redis:unicum_redis \
-    dockerfile/redis \
+    redis \
     bash -c 'redis-cli -h "$UNICUM_REDIS_PORT_6379_TCP_ADDR"'
